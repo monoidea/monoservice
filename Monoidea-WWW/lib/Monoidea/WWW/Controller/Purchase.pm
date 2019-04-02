46a4e290e9318bd9ca62848c7af3dc8227796514
Joël Krähemann <jkraehemann@gmail.com>
2019-04-03 01:14:12 +0200

    fixed some errors in purchase controller

--- Monoidea-WWW/lib/Monoidea/WWW/Controller/Purchase.pm
+++ Monoidea-WWW/lib/Monoidea/WWW/Controller/Purchase.pm
@@ -33,13 +33,15 @@ sub prepare :Local {
 
     if($c->user_exists() && $c->check_user_roles( qw / can_transaction / )){
 	my $session_id = $c->req->params->{'session_id'};
+	my $token = $c->req->params->{'token'};
 	my $media_account_id = $c->req->params->{'media_account_id'};
 
-	my $product = $c->req->params->{'product'};
+	my $product_name = $c->req->params->{'product_name'};
 	my $position_id = $c->req->params->{'position_id'};
 	my $recipe_id = $c->req->params->{'recipe_id'};
 	my $invoice_amount = $c->req->params->{'invoice_amount'};
 
+	# get some record sets
 	my $session_store_rs = $c->model('MONOSERVICE::SessionStore');
 	my $media_account_rs = $c->model('MONOSERVICE::MediaAccount');
 	my $billing_address_rs = $c->model('MONOSERVICE::BillingAddress');
@@ -48,12 +50,13 @@ sub prepare :Local {
 	my $payment_rs = $c->model('MONOSERVICE::Payment');
 	my $purchase_rs = $c->model('MONOSERVICE::Purchase');
 
+	# prepare
 	my $current_time = time();
 
-	my $product = $product_rs->find({ product_name => $product});
+	my $product = $product_rs->find({ product_name => $product_name});
 
 	my $payment = $payment_rs->create({ recipe_id => $recipe_id,
-					    invoice_amount => $amount,
+					    invoice_amount => $invoice_amount,
 					    due_date => $current_time});
 
 	my $purchase = $purchase_rs->create({ position_id => $position_id,
@@ -61,7 +64,8 @@ sub prepare :Local {
 					      product => $product->product_id,
 					      billing_time => $current_time });
 
-	my $session;
+	# find billing address, media account and session
+	my $session_store;
 	my $media_account;
 	my $billing_address;
 
@@ -71,12 +75,17 @@ sub prepare :Local {
 
 	    $billing_address = $billing_address_rs->find({ billing_address_id => $media_account->billing_address });
 
-	    $session = $session_store_rs->find({ media_account => $media_account_id });
+	    $session_store = $session_store_rs->find({ media_account => $media_account_id });
 	}
 
-	if(!$session ||
+	# create billing address, media account and session
+	if(!$session_store ||
 	   !$media_account ||
-	   !$billing_address){
+	   !$billing_address ||
+	   !$session_store->session_id ||
+	   !$session_store->token ||
+	    $session_store->session_id != $session_id ||
+	    $session_store->token != $token){
 	    $billing_address = $billing_address_rs->create({ });
 
 	    $media_account = $media_account_rs->create({ billing_address => $billing_address->billing_address_id });
@@ -87,11 +96,15 @@ sub prepare :Local {
 	    $session_id = $session_id_guid->as_string;
 	    $token = $token_guid->as_string;
 
-	    $session = $session_store_rs->create({ media_account => $media_account->media_account_id,
-						   session_id => $session_id,
-						   token => $token });
+	    $session_store = $session_store_rs->create({ media_account => $media_account->media_account_id,
+							 session_id => $session_id,
+							 token => $token });
+	}else{
+	    $session_id = $session_store->session_id;
+	    $token = $session_store->token;
 	}
 
+	# create response
 	my $dom = XML::LibXML::Document->new('1.0', 'UTF-8');
 
 	my $root_node = $dom->createElement('monoidea-order');
@@ -114,6 +127,15 @@ sub prepare :Local {
 	$node = $dom->createElement('payment');
 	$node->appendText($payment->payment_id);
 	$root_node->appendChild($node);
+
+	# write response
+	my $response_body = $dom.toString();
+
+	$c->response->headers->content_type('text/xml');
+	$c->response->headers->content_length(length($response_body));
+	$c->response->headers->last_modified($current_time);
+
+	$c->response->body($response_body);
     }else{
 	$c->detach("access_denied");
     }
@@ -127,7 +149,7 @@ sub cancel :Local {
 
 	my $payment_rs = $c->model('MONOSERVICE::Payment');
 
-	$payment = $payment_rs->find({ payment_id => $payment_id });
+	my $payment = $payment_rs->find({ payment_id => $payment_id });
 
 	$payment->update({ due_date => undef });
     }else{
@@ -139,6 +161,8 @@ sub completed :Local {
     my ( $self, $c ) = @_;
 
     if($c->user_exists() && $c->check_user_roles( qw / can_transaction / )){
+	my $payment_id = $c->req->params->{'payment_id'};
+
 	my $payment_rs = $c->model('MONOSERVICE::Payment');
 
 	my $payment = $payment_rs->find({ payment_id => $payment_id });
